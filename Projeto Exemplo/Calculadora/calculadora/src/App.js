780f901203ff214bfdfb8fd144035156930ba9f0
Lucas dos Santos Amorim Rêgo <lucass235@gmail.com>
2022-01-19 17:19:57 -0300

    Calculadora final

--- Projeto Exemplo/Calculadora/calculadora/src/App.js
+++ Projeto Exemplo/Calculadora/calculadora/src/App.js
@@ -1,59 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
-
-class numero extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: "" };
-  }
-  
-  render() {
-    return (
-      <div>
-      <button valor={this.props.valor}>{this.props.valor}</button>
-      </div>
-    );
-  }
-}
+import Calculadora from './components/Calculadora';
 
 class App extends Component {
- render() {
+
+  render() {
   return (
     <div className="container">
-     <h2> Calculadora</h2>
-     <table>
-      <tr> 
-      <td> <button>7</button> </td>
-        <td> <button>8</button> </td>
-        <td> <button>9</button> </td>
-      </tr>
-
-      <tr> 
-        <td> <button>4</button> </td>
-        <td> <button>5</button> </td>
-        <td> <button>6</button> </td>
-        </tr>
-
-        <tr>
-         <td> <button> 0</button> </td>
-          <td> <button>1 </button> </td>
-          <td> <button> 2</button> </td>
-          <td> <button> 3</button> </td>
-        </tr>
-
-        <tr> 
-          <td><button> +</button></td>
-          <td><button> -</button></td>
-          <td><button> *</button></td>
-          <td><button> /</button></td>
-        </tr>
-        
-     </table>
-     
+     <Calculadora />
     </div>
   );
 }
 }
 
-export default App;
+export default App; 
