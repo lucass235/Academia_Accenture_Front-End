9c86d359e93755e4cbe172631c296792829fbd66
Lucas dos Santos Amorim Rêgo <lucass235@gmail.com>
2022-01-24 20:28:47 -0300

    .

---
+++ Projeto Exemplo/exemplo06/aula06/src/components/Form/index.jsx
@@ -0,0 +1,14 @@
+import * as S from "./styles";
+import { useState } from "react";
+import { useFormik} from "formik";
+
+function Form() {
+  
+  return (
+    <S.FormBox>
+     
+    </S.FormBox>
+  );
+}
+
+export default Form;
